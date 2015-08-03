2d674f997edfb3c48f0cba6edab6b6ca7a19a2ae
trabbissue <hoge@huga.com>
2015-08-03 15:57:23 +0900

    add .emacs.d/config/load-paths.el

---
+++ .emacs.d/config/load-paths.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path "~/.emacs.d/config/interfaces")
+(add-to-list 'load-path "~/.emacs.d/config/keybindings")
+
+(require 'interface)
+(require 'keybind)
+
+(provide 'load-paths)
