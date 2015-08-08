78a504229eebae8ebbe0ebe6848533d4339be727
trabbissue <hoge@huga.com>
2015-08-09 01:11:10 +0900

    update fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,12 @@
 set -x PATH $HOME/.pyenv/bin $HOME/.pyenv/shims $PATH
 pyenv rehash > /dev/null ^&1
 
-#rbenv
+# rbenv
 set -x PATH $HOME/.rbenv/bin $HOME/.rbenv/shims $PATH
 rbenv rehash > /dev/null ^&1
+
+# mysql
+set -x PATH /usr/local/mysql/bin /usr/local/mysql/support-files $PATH
 
 # ctags -> exuberant ctags
 function ctags
