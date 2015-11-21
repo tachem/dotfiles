169ba25701427df2f2d588e6dfb488e80a2d2cf4
trabbissue <hoge@huga.com>
2015-11-21 13:03:13 +0900

    caskのファイルパスを修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,5 @@
 ;; cask
-;(require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
-(require 'cask "/usr/local/Cellar/cask/0.7.2_1/share/emacs/site-lisp/cask/cask.el")
+(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
 (cask-initialize)
 
 ;; load path
