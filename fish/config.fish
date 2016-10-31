4a906fcbae8cd64c8bc7d6dca1377045e64639a5
tsukachu <concon.ch.1543@gmail.com>
2016-10-31 12:06:04 +0900

    *env周り更新

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,14 @@
 # pyenv
-set -x PATH $HOME/.pyenv/bin $HOME/.pyenv/shims $PATH
-pyenv rehash > /dev/null ^&1
+status --is-interactive; and source (pyenv init -|psub)
 
 # rbenv
-set -x PATH $HOME/.rbenv/bin $HOME/.rbenv/shims $PATH
-rbenv rehash > /dev/null ^&1
+status --is-interactive; and . (rbenv init -|psub)
 
 # nodebrew
 set -x PATH $HOME/.nodebrew/current/bin $PATH
+
+# direnv
+eval (direnv hook fish)
 
 # sed -> gnu_sed
 function sed
